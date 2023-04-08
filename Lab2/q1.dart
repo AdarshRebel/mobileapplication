ea8732e42c0d9100ced97f98a55b55c20cca7311
AdarshRebel <alokyadav1139@gmail.com>
2023-04-08 18:07:33 +0545

    lab2 qn 1 added

---
+++ Lab2/q1.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Create a list of names
+  List<String> names = ['Manoj', 'Sushant', 'Jems', 'Bore', 'Anoj'];
+
+  // Print each name in the list
+  for (String name in names) {
+    print(name);
+  }
+}
